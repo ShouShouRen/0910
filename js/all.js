4ea744830697b8a0ec2f31cf2ee199f68190cbb3
Peter <peterchen2017125@gmail.com>
2022-09-15 18:38:50 +0800

    20220915

---
+++ js/all.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    // $(window).scroll(function () {
+    //     var scroll = $(window).scrollTop();
+    //     if (scroll >= 120) {
+    //         $('.navbar').addClass('bg-op')
+    //     } else {
+    //         $('.navbar').removeClass('bg-op')
+    //     }
+    // })
+    $('.nav-link,a').click(function () {
+        var t = $(this).attr('href');
+        var st = $(t).offset().top - 90;
+        $('html,body').animate({
+            scrollTop: st
+        }, 500);
+    });
+});
---
+++ js/index.js
@@ -0,0 +1,60 @@
+Vue.createApp({
+    data() {
+        return {
+            products_1: [{
+                    title: '草莓蛋糕',
+                    img: '../images/product-a.jpg',
+                    price: 380,
+                    amountShow: 0,
+                },
+                {
+                    title: '草莓布朗尼',
+                    img: '../images/product-b.jpg',
+                    price: 60,
+                    amountShow: 0,
+                },
+                {
+                    title: '巧克力甜甜圈',
+                    img: '../images/product-c.jpg',
+                    price: 40,
+                    amountShow: 0,
+                },
+            ],
+            products_2: [
+                {
+                    title: '芝麻麵包',
+                    img: '../images/product-d.jpg',
+                    price: 70,
+                    amountShow: 0,
+                },
+                {
+                    title: '堅果吐司',
+                    img: '../images/product-e.jpg',
+                    price: 150,
+                    amountShow: 0,
+                },
+                {
+                    title: '手做麵包',
+                    img: '../images/product-f.jpg',
+                    price: 50,
+                    amountShow: 0,
+                },
+            ]
+        }
+    },
+    methods: {
+        minus(product) {
+            product.amountShow--
+            product.amountShow = (product.amountShow < 1) ? 0 : product.amountShow
+        },
+        plus(product) {
+            product.amountShow++
+            product.amountShow = (product.amountShow < 1) ? 0 : product.amountShow
+        },
+    },
+    computed: {
+        sub() {
+            return this.products.map(data => data.price * data.amountShow)
+        }
+    }
+}).mount("#app")

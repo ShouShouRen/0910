73b6635271b9be0833e97d70e51309ef0855d683
Peter Chen <peterchen2017125@gmail.com>
2022-09-19 11:58:47 +0800

    20220919

--- js/all.js
+++ js/all.js
@@ -1,12 +1,4 @@
 $(document).ready(function () {
-    // $(window).scroll(function () {
-    //     var scroll = $(window).scrollTop();
-    //     if (scroll >= 120) {
-    //         $('.navbar').addClass('bg-op')
-    //     } else {
-    //         $('.navbar').removeClass('bg-op')
-    //     }
-    // })
     $('.nav-link,a').click(function () {
         var t = $(this).attr('href');
         var st = $(t).offset().top - 90;
